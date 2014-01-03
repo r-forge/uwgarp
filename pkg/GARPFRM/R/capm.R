464a3b9f18f6875f4621ef3f0a2566bcd60aad30
rossbennett34 (Ross Bennett) <rossbennett34@gmail.com>
2014-01-03 20:20:14 +0000

    Adding capm functions and test in sandbox
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uwgarp@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/GARPFRM/R/capm.R
+++ pkg/GARPFRM/R/capm.R
@@ -1,22 +1,94 @@
-#' CAPM Function
+# CAPM Function 
+# Description for CAPM
+# @param r risk-free rate
+# @param mkrt market return
+# @return the function returns tstat upon default & pvalue when specified
+# @export
+# capm.tstats = function(r,mkrt,type = FALSE) {
+#   # Fiting CAPM and retrieve alpha specific tstats or pvalues
+#   capm.fit = lm(r~mkrt)    
+#   # Extract summary info
+#   capm.summary = summary(capm.fit) 
+#   if(is.null(type) | type=="pvalue"){
+#     # Retrieve p-value if specified
+#     p.value = coef(capm.summary)[1,4]  
+#     p.value
+#   }else{
+#     # Otherwise retrieve t-stat if specified or on default
+#     t.stat = coef(capm.summary)[1,3]  
+#     t.stat
+#   }
+# }
+
+#' Capital Asset Pricing Model
 #' 
-#' Description for CAPM
-#' @param r risk-free rate
-#' @param mkrt market return
-#' @return the function returns tstat upon default & pvalue when specified
+#' Description of CAPM
+#' 
+#' @param R asset returns
+#' @param Rmkt market returns
 #' @export
-capm.tstats = function(r,mkrt,type = FALSE) {
-  # Fiting CAPM and retrieve alpha specific tstats or pvalues
-  capm.fit = lm(r~mkrt)    
-  # Extract summary info
-  capm.summary = summary(capm.fit) 
-  if(is.null(type) | type=="pvalue"){
-    # Retrieve p-value if specified
-    p.value = coef(capm.summary)[1,4]  
-    p.value
-  }else{
-    # Otherwise retrieve t-stat if specified or on default
-    t.stat = coef(capm.summary)[1,3]  
-    t.stat
-  }
+CAPM <- function(R, Rmkt){
+  # We should have a capm_uv class for a univariate capm (i.e. R is the returns
+  # of a single asset) and capm_mv for a multivariate capm (i.e. R is the returns
+  # for multiple assets)
+  
+  # if(ncol(R) > 1){
+  #   multivariate capm
+  # } else if(ncol(R) == 1){
+  #   univariate capm
+  # }
+  
+  # For now, assume that R is the returns of a single asset
+  capm_fit <- lm(R ~ Rmkt)
+  class(capm_fit) <- c("capm_uv", "lm")
+  return(capm_fit)
+}
+
+#' CAPM alphas
+#' 
+#' Description of CAPM alphas
+#' 
+#' @param object a capm object created by \code{\link{CAPM}}
+#' @export
+getAlphas <- function(object){
+  UseMethod("getAlphas")
+}
+
+#' @method getAlphas capm_uv
+#' @S3method getAlphas capm_uv
+getAlphas.capm_uv <- function(object){
+  return(coef(object)[1])
+}
+
+#' CAPM betas
+#' 
+#' Description of CAPM betas
+#' 
+#' @param object a capm object created by \code{\link{CAPM}}
+#' @export
+getBetas <- function(object){
+  UseMethod("getBetas")
+}
+
+#' @method getBetas capm_uv
+#' @S3method getBetas capm_uv
+getBetas.capm_uv <- function(object){
+  return(coef(object)[2])
+}
+
+#' CAPM statistics
+#' 
+#' Description of CAPM statistics (standard error, t-values, and p-values)
+#' @param object a capm object created by \code{\link{CAPM}}
+#' @export
+getStatistics <- function(object){
+  UseMethod("getStatistics")
+}
+
+#' @method getStatistics capm_uv
+#' @S3method getStatistics capm_uv
+getStatistics.capm_uv <- function(object){
+  tmp_sm <- summary.lm(object)
+  # gets the standard error, t-value, and p-value of model
+  return(coef(tmp_sm)[,2:4])
 }
---
+++ pkg/GARPFRM/sandbox/test_capm_rb.R
@@ -0,0 +1,25 @@
+
+library(GARPFRM)
+data(crsp.short)
+
+head(largecap.ts)
+
+Rf <- largecap.ts[, "t90"]
+R <- largecap.ts[, "CAT"] - Rf
+MKT <- largecap.ts[, "market"] - Rf
+
+# Fit the CAPM model
+tmp <- CAPM(R=R, Rmkt=MKT)
+
+class(tmp)
+
+# Internally, R is calling print.lm and summary.lm because tmp is of class lm
+# You can define your own print.capm_uv and summary.capm_uv for control over
+# what values are calculated and the output that is displayed.
+print(tmp)
+summary(tmp)
+
+# Demonstrate extractor functions
+getAlphas(tmp)
+getBetas(tmp)
+getStatistics(tmp)
