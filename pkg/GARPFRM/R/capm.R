3956af523436c00014d247029b48c3546bc8264b
tfillebeen (Thomas Fillebeen) <tdf17@uw.edu>
2014-01-03 05:29:42 +0000

    Cleanup
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uwgarp@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/GARPFRM/R/capm.R
+++ pkg/GARPFRM/R/capm.R
@@ -1,16 +1,22 @@
 #' CAPM Function
 #' 
 #' Description for CAPM
-#' @param rf risk-free rate
-#' @param mkrt
-#' @return this is what the function returns
+#' @param r risk-free rate
+#' @param mkrt market return
+#' @return the function returns tstat upon default & pvalue when specified
 #' @export
-capm.tstats = function(rf,mkrt) {
-  # Fiting CAPM
-  capm.fit = lm(rf~mkrt)  	
+capm.tstats = function(r,mkrt,type = FALSE) {
+  # Fiting CAPM and retrieve alpha specific tstats or pvalues
+  capm.fit = lm(r~mkrt)    
   # Extract summary info
-  capm.summary = summary(capm.fit)		
-  # Retrieve t-stat
-  t.stat = coef(capm.summary)[1,3]	
-  t.stat
+  capm.summary = summary(capm.fit) 
+  if(is.null(type) | type=="pvalue"){
+    # Retrieve p-value if specified
+    p.value = coef(capm.summary)[1,4]  
+    p.value
+  }else{
+    # Otherwise retrieve t-stat if specified or on default
+    t.stat = coef(capm.summary)[1,3]  
+    t.stat
+  }
 }
