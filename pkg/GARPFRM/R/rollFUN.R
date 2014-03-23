d7acd706d118fe3be838dd66369e2ecebaed8c62
rossbennett34 (Ross Bennett) <rossbennett34@gmail.com>
2014-03-23 16:26:12 +0000

    Revising rollCor and rollCov to handle any number of columns
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uwgarp@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/GARPFRM/R/rollFUN.R
+++ pkg/GARPFRM/R/rollFUN.R
@@ -1,22 +1,9 @@
 
-#' Rolling Covariance Estimate
-#' 
-#' This function calculates the covariance estimate of the returns of two 
-#' assets over a rolling window
-#' 
-#' @param R xts or zoo object of asset returns
-#' @param width width of rolling window
-#' @author Ross Bennett
-#' @seealso \code{\link{cov}}
-#' @examples
-#' data(crsp_weekly)
-#' R <- largecap_weekly[,1:2]
-#' tail(rollCov(R, 10))
-#' @export
-rollCov <- function(R, width){
-  if(!inherits(R, c("xts", "zoo"))) stop("x must be an xts or zoo object")
+.rollCov <- function(R, width){
+  # if(!inherits(R, c("xts", "zoo"))) stop("x must be an xts or zoo object")
+  # R should be an xts object with 2 columns
   n <- nrow(R)
-  out <- xts(vector("numeric", n), index(R))
+  out <- vector("numeric", n)
   for(i in width:n){
     tmpR <- R[(i-width+1):i,]
     out[i] <- cov(tmpR[,1], tmpR[,2])
@@ -28,24 +15,51 @@ rollCov <- function(R, width){
   out
 }
 
-#' Rolling Correlation Estimate
+#' Rolling Covariance Estimate
 #' 
-#' This function calculates the correlation estimate of the returns of two 
-#' assets over a rolling window
+#' This function calculates the covariance estimate between the returns of a 
+#' pair of assets over a rolling window.
 #' 
 #' @param R xts or zoo object of asset returns
 #' @param width width of rolling window
 #' @author Ross Bennett
-#' @seealso \code{\link{cor}}
+#' @seealso \code{\link{cov}}
 #' @examples
 #' data(crsp_weekly)
-#' R <- largecap_weekly[,1:2]
-#' tail(rollCor(R, 10))
+#' R <- largecap_weekly[,1:4]
+#' tail(rollCov(R, 10))
 #' @export
-rollCor <- function(R, width){
+rollCov <- function(R, width){
   if(!inherits(R, c("xts", "zoo"))) stop("x must be an xts or zoo object")
+  if(ncol(R) < 2) stop("R must have 2 or more columns of asset returns")
+  
+  cnames <- colnames(R)
+  if(ncol(R) == 2){
+    out <- .rollCov(R=R, width=width)
+    out_names <- paste(cnames[1], cnames[2], sep=".")
+  } else if(ncol(R) > 2){
+    out <- matrix(0, nrow=nrow(R), ncol=choose(ncol(R), 2))
+    out_names <- vector("numeric", ncol(out))
+    k <- 1
+    for(i in 1:(ncol(R)-1)){
+      for(j in (i+1):ncol(R)){
+        out[, k] <- .rollCov(R=cbind(R[,i], R[,j]), width=width)
+        out_names[k] <- paste(cnames[i], cnames[j], sep=".")
+        k <- k + 1
+      }
+    }
+  }
+  # convert to xts and return
+  out <- xts(out, index(R))
+  colnames(out) <- out_names
+  return(out)
+}
+
+.rollCor <- function(R, width){
+  if(!inherits(R, c("xts", "zoo"))) stop("x must be an xts or zoo object")
+  # R should be an xts object with 2 columns
   n <- nrow(R)
-  out <- xts(vector("numeric", n), index(R))
+  out <- vector("numeric", n)
   for(i in width:n){
     tmpR <- R[(i-width+1):i,]
     out[i] <- cor(tmpR[,1], tmpR[,2])
@@ -57,13 +71,52 @@ rollCor <- function(R, width){
   out
 }
 
+#' Rolling Correlation Estimate
+#' 
+#' This function calculates the correlation estimate between the returns of a 
+#' pair of assets over a rolling window.
+#' 
+#' @param R xts or zoo object of asset returns
+#' @param width width of rolling window
+#' @author Ross Bennett
+#' @seealso \code{\link{cor}}
+#' @examples
+#' data(crsp_weekly)
+#' R <- largecap_weekly[,1:4]
+#' tail(rollCor(R, 10))
+#' @export
+rollCor <- function(R, width){
+  if(!inherits(R, c("xts", "zoo"))) stop("x must be an xts or zoo object")
+  if(ncol(R) < 2) stop("R must have 2 or more columns of asset returns")
+  
+  cnames <- colnames(R)
+  if(ncol(R) == 2){
+    out <- .rollCor(R=R, width=width)
+    out_names <- paste(cnames[1], cnames[2], sep=".")
+  } else if(ncol(R) > 2){
+    out <- matrix(0, nrow=nrow(R), ncol=choose(ncol(R), 2))
+    out_names <- vector("numeric", ncol(out))
+    k <- 1
+    for(i in 1:(ncol(R)-1)){
+      for(j in (i+1):ncol(R)){
+        out[, k] <- .rollCor(R=cbind(R[,i], R[,j]), width=width)
+        out_names[k] <- paste(cnames[i], cnames[j], sep=".")
+        k <- k + 1
+      }
+    }
+  }
+  # convert to xts and return
+  out <- xts(out, index(R))
+  colnames(out) <- out_names
+  return(out)
+}
+
 # rollSD function for a univariate R
 .rollSD <- function(R, width){
   # if(!inherits(R, c("xts", "zoo"))) stop("R must be an xts or zoo object")
   # this function should generally not be called by the user and we will check
   # for xts or zoo object in rollSD which calls .rollSD
   n <- length(R)
-  # out <- xts(vector("numeric", n), index(R))
   out <- vector("numeric", n)
   for(i in width:n){
     tmpR <- R[(i-width+1):i,1]
